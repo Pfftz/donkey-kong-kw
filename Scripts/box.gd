5b61f7d1b5cf54a8af3402314afd2fba2ba26416
Pfftz <Kalixpfft+github@protonmail.com>
2024-10-28 23:00:03 +0700

    Part 11 belum kelar

---
+++ Scripts/box.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+
+func _on_body_entered(body:Node2D):
+	pass # Replace with function body.
+
+
+func _on_animated_sprite_2d_animation_finished():
+	pass # Replace with function body.
